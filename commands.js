af0e86425283a7d23798e6be62d9b8c9e8662692
CJE90 <mrktup@hotmail.com>
2021-07-24 22:44:00 -0400

    fixing config issue

--- commands.js
+++ commands.js
@@ -1,4 +1,4 @@
-const { prefix } = require('./config.json');
+//const { prefix } = require('./config.json');
 
 module.exports = (client, aliases, callback) => {
 
@@ -10,7 +10,7 @@ module.exports = (client, aliases, callback) => {
         const { content } = message;
 
         aliases.forEach(alias => {
-            const command = `${prefix}${alias}`;
+            const command = `!${alias}`;
 
             if (content.startsWith(`${command}`) || content === command) {
                 console.log(`Running the command ${command}`);
