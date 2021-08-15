cc7c14d4dbc35595f640f98068d0113d6c33a34c
CJE90 <mrktup@hotmail.com>
2021-08-14 23:58:05 -0400

    removing config requirement

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const config = require('./config.json');
+//const config = require('./config.json');
 const client = new Discord.Client();
 const mongo = require('./mongo.js');
 const command = require('./command.js');
